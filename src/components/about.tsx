562fe195ab1c01cb477e6432bfea433bccc6ebbf
omar alsyouf <omaralsoyouf@gmail.com>
2024-01-17 11:28:36 +0000

    update my bio

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -33,15 +33,31 @@ export const About = () => {
         <div>
           <h2>Bio</h2>
           <p>
-            I am a Full-Stack Software Developer that has graduated from a
-            six-month Software Engineering academy with a particular interest in
-            making things simple.
+            I am a Full-Stack Software Developer with a comprehensive background
+            in Software Engineering, holding both a Software Engineering academy
+            certification and a Bachelor's degree in Mathematics. Alongside my
+            technical skills, I have obtained a designing certificate,
+            showcasing my versatility in creating user-friendly and
+            aesthetically pleasing applications.
             <br />
-            <br />I have experience creating full-stack applications using
-            NextJS, Strapi, Webflow, React, NodeJS and PostgreSQL stack. I try
-            to keep up with security and best practices and I am always looking
-            for new things to learn as I like to keep myself busy on a daily
-            basis.
+            <br />
+            My approach to development revolves around the philosophy of
+            simplicity, striving to make complex concepts and applications more
+            accessible. In my journey, I have successfully developed full-stack
+            applications using a diverse tech stack, including NextJS, Strapi,
+            Webflow, ReactJS, NodeJS, and PostgreSQL. I am dedicated to
+            maintaining the highest standards of security and best practices in
+            my projects.
+            <br />
+            <br />
+            Constantly seeking opportunities for growth and learning, I am known
+            for my proactive nature, always eager to explore new technologies
+            and methodologies. This curiosity and commitment to staying abreast
+            of the latest industry trends contribute to my dynamic and evolving
+            skill set. I thrive on keeping myself engaged in learning and
+            applying new knowledge, making each day a chance to further enhance
+            my proficiency and contribute meaningfully to the world of software
+            development.
           </p>
         </div>
       </div>
